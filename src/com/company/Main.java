b3b0558d87dab4f4dedc6485db936b48233ca3e0
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-04 18:57:48 -0500

    fullname

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,13 @@ public class Main {
 	   String firstName = "Olubunmi ";
        String lastName = "Osunde";
        String fullName = firstName + lastName ;
+       String createFullName = new String();
         System.out.println(fullName);
+        System.out.println();
+    }
+    public void createFullName (String[] args) {
+        String firstName = "Gabriel";
+        String lastName = "Sherman";
 
     }
 }
