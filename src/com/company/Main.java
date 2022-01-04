9f18a751a30b764225df073f629a747e087a1842
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-03 20:53:02 -0500

    fullname

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    
+	   String firstName = "Olubunmi ";
+       String lastName = "Osunde";
+       String fullName = firstName + lastName ;
+        System.out.println(fullName);
 
     }
 }
